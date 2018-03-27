5a6658f8a357d031cfa75f7c2dfb737b0356c783
Agustín Ventura <agustinventura@gmail.com>
2018-03-27 20:13:12 +0200

    adds preferences form

--- src/app/components/confirm/confirm.component.ts
+++ src/app/components/confirm/confirm.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../model/user';
+import { LoginService } from '../../services/login.service';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-confirm',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirm.component.css']
 })
 export class ConfirmComponent implements OnInit {
+  user: User = null;
+  preferences: FormGroup = null;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private loginService: LoginService, private formBuilder: FormBuilder) {
+    this.createForm();
   }
 
+  createForm() {
+    this.preferences = this.formBuilder.group({
+      accompanied: false,
+      children: false
+    });
+  }
+
+  ngOnInit() {
+    this.user = this.loginService.user;
+  }
 }
