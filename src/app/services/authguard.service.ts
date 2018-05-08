e1a671651a373b677032e89bf02ff3df54d423b9
Agustín Ventura <agustinventura@gmail.com>
2018-05-08 11:52:37 +0200

    adds admin check for access to admin urls

--- src/app/services/authguard.service.ts
+++ src/app/services/authguard.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 
 import { LoginService } from './login.service';
 
 @Injectable()
 export class AuthguardService implements CanActivate {
 
+  adminUrls = ['admin', 'user-form'];
+
   constructor(private loginService: LoginService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route: ActivatedRouteSnapshot) {
     if (this.loginService.user) {
       if (this.loginService.user.enabled) {
-        return true;
+        const url = route.url[0];
+        if (this.adminUrls.includes(url.path) && !this.loginService.user.admin) {
+          return false;
+        } else {
+          return true;
+        }
       } else {
         this.router.navigate(['user-disabled']);
         return false;
